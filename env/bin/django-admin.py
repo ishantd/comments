1ae51ee41d971dd62774bedd1994240d8db26e3e
Ishant <ishantdahiya@gmail.com>
2020-06-23 02:26:51 +0530

    final

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ishant/ishant_linux/pytholabs/django-test/finalcomments/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- finalcomments/settings.py
+++ finalcomments/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,15 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'pytholabs',
     'django_comments_xtd',
     'django_comments',
-    'pytholabs',
 ]
 
 SITE_ID = 1
-
 COMMENTS_APP = 'django_comments_xtd'
 COMMENTS_XTD_MAX_THREAD_LEVEL = 2
+COMMENTS_XTD_CONFIRM_EMAIL = False
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
